d9c58ad167aa391be4c465e6c6c21148a7bde678
Kaung Min Khant <kaung.minkhant@haulio.io>
2023-05-18 16:24:08 +0700

    Updates

---
+++ precache-manifest.013666e446bcc0b06ffed9aa39701b6c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac34528a7d51130f06ac55056457db6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a69715043f1a1a2448d1",
+    "url": "/static/css/main.eab6f821.chunk.css"
+  },
+  {
+    "revision": "fdfc9a0d4f190908b7ae",
+    "url": "/static/js/2.dda452c1.chunk.js"
+  },
+  {
+    "revision": "2e7e5a466ae113f07513391c0542446b",
+    "url": "/static/js/2.dda452c1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a69715043f1a1a2448d1",
+    "url": "/static/js/main.768980c9.chunk.js"
+  },
+  {
+    "revision": "920bdda65fe45f360b1b",
+    "url": "/static/js/runtime-main.ce6d1b4d.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/static/media/logo.19ef7b17.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-cart.30f07071.svg"
+  }
+]);
